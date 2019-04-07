fdff0e3cde20bfe243923fad5053e0b0bce784fe
angadsharma1016 <angadsharma1016@gmail.com>
2019-04-08 01:06:27 +0530

    docs corrected

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,13 +1,19 @@
 define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
+  "name": "Grofers task documentation",
+  "version": "0.1.0",
+  "description": "For the grofers",
+  "title": "docs",
+  "url": "http://localhost:3000/api/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-07T19:21:41.742Z",
+    "time": "2019-04-07T19:36:05.210Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
